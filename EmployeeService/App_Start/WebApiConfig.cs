01b9b8e5ebb359a8c78c743963a54c4a190739b9
Riyad <eng.riyadalali93@gmail.com>
2021-04-20 10:31:51 +0300

    Add project files.

---
+++ EmployeeService/App_Start/WebApiConfig.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+
+namespace EmployeeService
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Web API configuration and services
+
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+
+            //remove XML Formater and it will always return json data
+            config.Formatters.Remove(config.Formatters.XmlFormatter);
+
+            //-> to return only xml format 
+          //  config.Formatters.Remove(config.Formatters.JsonFormatter);
+
+            // config.Formatters.JsonFormatter.SerializerSettings.Formatting = Newtonsoft.Json.Formatting.Indented;  // to manage the received json data and orgainse it
+
+
+
+
+        }
+    }
+}
---
+++ EmployeeService/Controllers/EmployeeController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using EmployeeDataAccess;
+
+namespace EmployeeService.Controllers
+{
+    public class EmployeeController : ApiController
+    {
+        public IEnumerable<Employee> Get()
+        {
+            using (EmployeeAPIEntities entities = new EmployeeAPIEntities())
+            {
+               return entities.Employees.ToList();
+            }
+        }
+
+        public HttpResponseMessage Get(int ID)
+        {
+            using (EmployeeAPIEntities entities = new EmployeeAPIEntities())
+            {
+              var entity=  entities.Employees.FirstOrDefault(e=>e.ID==ID);
+                if(entity!=null)
+                {
+                    return Request.CreateResponse(HttpStatusCode.OK, entity);
+                }
+                else
+                {
+                    return Request.CreateErrorResponse(HttpStatusCode.NotFound, "Employee with ID" +
+                        ID.ToString() + "Not found");
+                }
+            }
+        }
+
+        //-> Post data to the server 
+        public HttpResponseMessage Post ([FromBody] Employee employee)
+        {
+            try { 
+            using (EmployeeAPIEntities entites = new EmployeeAPIEntities())
+            {
+                entites.Employees.Add(employee);
+                entites.SaveChanges();
+                    var message = Request.CreateErrorResponse(HttpStatusCode.Created, employee.ToString());
+                    message.Headers.Location =
+                   new Uri(Request.RequestUri + employee.ID.ToString());
+                    return message;
+
+                }
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ex);
+            }
+        }
+
+
+        //---------------------------------------Post Delete Method------------------------------------------------
+        public HttpResponseMessage Delete (int id)
+        {
+            try {
+                using (EmployeeAPIEntities entities = new EmployeeAPIEntities())
+                {
+                    var entity = entities.Employees.FirstOrDefault(e => e.ID == id);
+                    if (entity == null)
+                    {
+                        return Request.CreateErrorResponse(HttpStatusCode.NotFound, "Employee with id= " + id.ToString() + " Not Found");
+                    }
+                    else
+                    {
+                        entities.Employees.Remove(entity);
+                        entities.SaveChanges();
+                        return Request.CreateResponse(HttpStatusCode.OK,"Done Succesfully");
+                    }
+
+                }
+            }
+            catch(Exception ex)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ex);
+            }
+           
+        }
+
+
+    }
+}
